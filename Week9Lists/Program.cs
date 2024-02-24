2b31053bbc6693ed4baa2bbea3171832f1f7ba68
KristjanKaa <159649951+KristjanKaa@users.noreply.github.com>
2024-02-24 13:08:02 +0200

    Added my project

---
+++ Week9Lists/Program.cs
@@ -0,0 +1,60 @@
+﻿string folderPath = @"C:\DataMänguJaoks";
+string fileName = "shoppingList.txt";
+string filePath = Path.Combine(folderPath, fileName);
+List<string> myShoppingList = new List<string>();
+
+if(File.Exists(filePath))
+{
+    myShoppingList = GetItemsFromUser();
+    File.WriteAllLines(filePath, myShoppingList);
+}
+else
+{
+    File.Create(filePath).Close();
+    Console.WriteLine($"File {fileName} on loodud!");
+    myShoppingList = GetItemsFromUser();
+    File.WriteAllLines(filePath, myShoppingList);
+}
+
+
+
+
+static List<string> GetItemsFromUser()
+{
+    List<string> userList = new List<string>();
+
+    while (true)
+    {
+        Console.WriteLine("lisa toode (Lisa)/ Välju (Välju):");
+        string userChoice = Console.ReadLine();
+
+        if (userChoice == "Lisa")
+        {
+            Console.WriteLine("Lisa toode:");
+            string userItem = Console.ReadLine();
+            userList.Add(userItem);
+        }
+        else
+        {
+            Console.WriteLine("Headaega!");
+            break;
+        }
+    }
+    return userList;
+}
+
+static void ShowItemsFromList(List<string> someList)
+{
+    Console.Clear();
+
+    int listlength = someList.Count;
+    Console.WriteLine($"Te olete lisanud {listlength} toodet oma ostukorvi.");
+
+    int i = 1;
+    foreach (string item in someList)
+    {
+
+        Console.WriteLine($"{i}. {item}");
+        i++;
+    }
+}
